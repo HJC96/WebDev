2aea2d9e7a8a5fb218e920af81f75543a0f9d8a6
HJC96 <gkswlcjs2@naver.com>
2023-06-08 05:16:13 +0900

    CRUD

---
+++ b01/src/main/java/org/zerock/b01/upload/UploadFileDTO.java
@@ -0,0 +1,9 @@
+package org.zerock.b01.upload;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public class UploadFileDTO {
+    private List<MultipartFile> files;
+}
